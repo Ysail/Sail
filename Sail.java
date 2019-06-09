56998304ebc8bf549bb243e3e5243922f4604c97
Ysail <42119149+Ysail@users.noreply.github.com>
2019-06-09 21:58:43 +0800

    更新Sail.java
    第一次修改java文件

--- Sail.java
+++ Sail.java
@@ -2,5 +2,6 @@ public class Sail{
     public static void main(String[] args){
         System.out.println("Hello GitHub");
         System.out.println("Hello GitHub Sail.java")
+        System.out.println("Hello Word")
     }
 }
