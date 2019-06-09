48db98063051e2c48d98b477427933519c77aa38
Ysail <42119149+Ysail@users.noreply.github.com>
2019-06-09 21:56:49 +0800

    Update Sail.java

--- Sail.java
+++ Sail.java
@@ -1,5 +1,6 @@
 public class Sail{
     public static void main(String[] args){
-        System.out.println("Hello Git Hub.");
+        System.out.println("Hello GitHub");
+        System.out.println("Hello GitHub Sail.java")
     }
 }
