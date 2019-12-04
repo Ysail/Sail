855daec40d4b1caabfd3d60116e2f013ca5fd1db
Ysail <42119149+Ysail@users.noreply.github.com>
2019-12-04 19:02:38 +0800

    Delete Sail.java

--- Sail.java
+++
@@ -1,7 +0,0 @@
-public class Sail{
-    public static void main(String[] args){
-        System.out.println("Hello GitHub");
-        System.out.println("Hello GitHub Sail.java")
-        System.out.println("Hello Word")
-    }
-}
